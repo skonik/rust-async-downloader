fd3839cb0cde140243569ca09506ffd1e31b2637
Skonik <s.konik.job@gmail.com>
2021-10-10 14:19:27 +0300

    Add progress bar and move to async

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ CLI program to download files from specified url. Rust learning purpose
 `$ cargo run https://github.com/seanmonstar/reqwest ./test.html` 
 
 As a result content of the page will be saved under `test.html`.
+![downloader_showcase.gif](./img/downloader_showcase.gif)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::fs;
-use std::path;
+use std::fs::File;
+use std::io::Write;
 
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -10,16 +12,43 @@ struct Cli {
     path: std::path::PathBuf
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = Cli::from_args();
 
     println!("Download from url: {}", args.url);
-    let response_bytes = reqwest::blocking::get(args.url)?
-        .bytes()?;
+    let response = reqwest::get(args.url)
+        .await?;
 
-    println!("Saving file to: {}", args.path.display());
-    fs::write(args.path, response_bytes).expect("Unable to write file");
+    let total_size_option = response.content_length();
 
+    let total_size = match total_size_option {
+        Some(size) => size,
+        None => panic!("no response length!"),
+    };
+
+    let mut stream = response.bytes_stream();
+    println!("Saving file to: {}", &args.path.display());
+
+    let mut file = File::create("foo.txt")?;
+    let bar = ProgressBar::new(total_size);
+
+    bar.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+
+    let mut downloaded_length: u64 = 0;
+    while let Some(chunk) = stream.next().await {
+        let chunk_data = chunk.unwrap();
+
+        downloaded_length = downloaded_length + (chunk_data.len() as u64);
+
+        file.write(&chunk_data).or(Err("Chunk writing error"))?;
+        bar.set_position(downloaded_length);
+    }
+
+    bar.finish_with_message(format!("File saved under {} 📦", &args.path.display()));
     Ok(())
 }
