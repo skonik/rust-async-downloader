c56a52eca3114434d6af7cad36922c5fd30826b9
Skonik <s.konik.job@gmail.com>
2021-10-15 19:50:55 +0300

    Move cli related part into another module

---
+++ src/cli/args.rs
@@ -0,0 +1,37 @@
+use std::fmt;
+use regex::Regex;
+use structopt::StructOpt;
+
+struct URLParsingError;
+
+impl fmt::Display for URLParsingError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "URL has invalid format!")
+    }
+}
+
+impl fmt::Debug for URLParsingError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{{ file: {}, line: {} }}", file!(), line!())
+    }
+}
+
+
+fn validate_url(url: &str) -> Result<String, URLParsingError> {
+    let url_regex = Regex::new(r"^https?://.*$").unwrap();
+
+    let result = match url_regex.is_match(url) {
+        true => Ok(url.to_string()),
+        false => Err(URLParsingError)
+    };
+
+    return result;
+}
+
+#[derive(StructOpt)]
+pub struct Cli {
+    #[structopt(parse(try_from_str = validate_url))]
+    pub url: String,
+    #[structopt(parse(from_os_str))]
+    pub path: std::path::PathBuf,
+}
