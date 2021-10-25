40c731035d985aa067a557fd54822f0cc9aab14f
Skonik <s.konik.dev@gmail.com>
2021-10-25 20:43:23 +0300

    Move entrypoint logic into processing and splitting functions

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
-use indicatif::{MultiProgress, ProgressBar};
-use structopt::StructOpt;
+use std::path::PathBuf;
 
 use futures_util::future::join_all;
-use std::path::PathBuf;
+use indicatif::{MultiProgress, ProgressBar};
+use structopt::StructOpt;
 
 mod cli;
 mod downloader;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = cli::args::Cli::from_args();
 
+async fn process_urls(urls: &Vec<String>, path: &std::path::PathBuf) {
     let multi_progress_bar = MultiProgress::new();
 
-    let urls: Vec<String> = args.url.split(',').map(|s| s.to_string()).collect();
 
     let mut futures = Vec::new();
-    for url in &urls {
+    for url in urls {
         futures.push(downloader::client::download(
             &url,
-            &args.path,
+            &path,
             &multi_progress_bar,
         ));
     }
 
     let joined_futures = join_all(futures);
     joined_futures.await;
+}
+
+fn split_urls(urls: &String) -> Vec<String> {
+    let urls_divided: Vec<String> = urls.split(',').map(|s| s.to_string()).collect();
+
+    return urls_divided;
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = cli::args::Cli::from_args();
+
+
+    let urls: Vec<String> = split_urls(&args.url);
+
+    process_urls(&urls, &args.path).await;
 
     Ok(())
 }
